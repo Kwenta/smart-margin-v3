25a498ea37923413db80e4b5480816c13271bd27
JaredBorders <jlbxsxs@gmail.com>
2023-08-07 15:53:15 -0400

    🗑️ Remove Events.sol

--- src/MarginEngine.sol
+++ src/MarginEngine.sol
@@ -1,10 +1,27 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
 
-import {Events} from "src/libraries/Events.sol";
 import {Multicallable} from "src/utils/Multicallable.sol";
 
-contract MarginEngine is Events, Multicallable {
+contract MarginEngine is Multicallable {
+    event AccountCreated(uint256 accountId);
+
+    event MarginDeposited(
+        uint256 accountId, address marginType, uint256 amount
+    );
+
+    event MarginWithdrawn(
+        uint256 accountId, address marginType, uint256 amount
+    );
+
+    event CollateralDeposited(
+        uint256 accountId, address collateralType, uint256 amount
+    );
+
+    event CollateralWithdrawn(
+        uint256 accountId, address collateralType, uint256 amount
+    );
+
     function createAccount(uint256 _desiredAccountId) external {
         emit AccountCreated(_desiredAccountId);
     }
