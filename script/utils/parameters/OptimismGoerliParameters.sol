457fa499d5e151a8460728d7131af991a0c8fbce
JaredBorders <jlbxsxs@gmail.com>
2023-08-07 15:52:37 -0400

    🚀 Update script params

---
+++ script/utils/parameters/OptimismGoerliParameters.sol
@@ -0,0 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.18;
+
+address constant OPTIMISM_GOERLI_PERPS_MARKET_PROXY =
+    0xf272382cB3BE898A8CdB1A23BE056fA2Fcf4513b;
---
+++ script/utils/parameters/OptimismParameters.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.18;
+
+address constant OPTIMISM_GOERLI_PERPS_MARKET_PROXY = address(0);
