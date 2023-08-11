dba5f49e2fdeb4750a65bba279d4ba6137a56efd
JaredBorders <jlbxsxs@gmail.com>
2023-08-11 13:26:58 -0400

    👷🏻‍♂️ Update libraries

--- src/libraries/Int128Lib.sol
+++ src/libraries/Int128Lib.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
 
-library Int256Lib {
+library Int128Lib {
     /// @notice get absolute value of the input, returned as an unsigned number.
     /// @param x: signed number
-    /// @return z uint256 absolute value of x
-    function abs(int256 x) internal pure returns (uint256 z) {
+    /// @return z uint128 absolute value of x
+    function abs(int128 x) internal pure returns (uint128 z) {
         assembly {
-            /// shr(255, x):
-            /// shifts the number x to the right by 255 bits:
-            /// IF the number is negative, the leftmost bit (bit 255) will be 1
-            /// IF the number is positive,the leftmost bit (bit 255) will be 0
+            /// shr(127, x):
+            /// shifts the number x to the right by 127 bits:
+            /// IF the number is negative, the leftmost bit (bit 127) will be 1
+            /// IF the number is positive,the leftmost bit (bit 127) will be 0
 
-            /// sub(0, shr(255, x)):
+            /// sub(0, shr(127, x)):
             /// creates a mask of all 1s if x is negative
             /// creates a mask of all 0s if x is positive
-            let mask := sub(0, shr(255, x))
+            let mask := sub(0, shr(127, x))
 
             /// If x is negative, this effectively negates the number
             // if x is positive, it leaves the number unchanged, thereby computing the absolute value
@@ -28,7 +28,7 @@ library Int256Lib {
     /// @param x: signed number
     /// @param y: signed number
     /// @return true if same sign, false otherwise
-    function isSameSign(int256 x, int256 y) internal pure returns (bool) {
+    function isSameSign(int128 x, int128 y) internal pure returns (bool) {
         assert(x != 0 && y != 0);
         return (x ^ y) >= 0;
     }
