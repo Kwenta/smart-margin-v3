aff6f35c7c1f11fca3115916a557c5e320d6dc29
JaredBorders <jlbxsxs@gmail.com>
2023-08-12 17:01:45 -0400

    🚀 Add script params

--- script/utils/parameters/OptimismGoerliParameters.sol
+++ script/utils/parameters/OptimismGoerliParameters.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
 
+/// @custom:todo make sure these are the correct addresses to point to as
+/// it is still a bit unclear what the final addresses will be or what
+/// proxies will/should be used
+
 address constant OPTIMISM_GOERLI_PERPS_MARKET_PROXY =
     0xf272382cB3BE898A8CdB1A23BE056fA2Fcf4513b;
 
