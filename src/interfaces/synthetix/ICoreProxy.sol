5703231d9cd969485c5ce7d5215d40da1efff2fd
JaredBorders <jlbxsxs@gmail.com>
2023-08-16 15:21:50 -0400

    👷🏻‍♂️ Add utility interface

--- src/interfaces/synthetix/ICoreProxy.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.18;
-
-interface ICoreProxy {
-    /// @notice calls Synthetix v3 core system to fetch
-    /// the address of the USD token
-    /// @return the address of the Synthetix v3 USD token
-    function getUsdToken() external view returns (address);
-}
