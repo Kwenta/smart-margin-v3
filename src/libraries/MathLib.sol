d0e5c877767babb0215a38eebceb8aa29a161e63
JaredBorders <jlbxsxs@gmail.com>
2023-08-29 16:34:16 -0400

    👷🏻‍♂️ Update library

--- src/libraries/MathLib.sol
+++ src/libraries/MathLib.sol
@@ -1,39 +1,31 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.18;
 
-/// @notice Arithmetic library with operations for fixed-point numbers.
-/// @author Solady (https://github.com/vectorized/solady/blob/main/src/utils/FixedPointMathLib.sol)
-/// @author Modified from Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/FixedPointMathLib.sol)
-library FixedPointMathLib {
-    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
-    /*                  GENERAL NUMBER UTILITIES                  */
-    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+/// @notice Math Library for int128 and int256
+library MathLib {
+    error CannotTakeAbsOfMinInt128();
+    error CannotTakeAbsOfMinInt256();
 
     /// @notice get absolute value of the input, returned as an unsigned number.
     /// @param x: signed number
     /// @return z uint128 absolute value of x
     function abs128(int128 x) internal pure returns (uint128 z) {
-        assembly {
-            /// shr(127, x):
-            /// shifts the number x to the right by 127 bits:
-            /// IF the number is negative, the leftmost bit (bit 127) will be 1
-            /// IF the number is positive,the leftmost bit (bit 127) will be 0
-
-            /// sub(0, shr(127, x)):
-            /// creates a mask of all 1s if x is negative
-            /// creates a mask of all 0s if x is positive
-            let mask := sub(0, shr(127, x))
-
-            /// If x is negative, this effectively negates the number
-            // if x is positive, it leaves the number unchanged, thereby computing the absolute value
-            z := xor(mask, add(mask, x))
+        if (x == type(int128).min) {
+            revert CannotTakeAbsOfMinInt128();
         }
+
+        /// @custom:todo can this be optimized similar to abs256?
+        z = x < 0 ? uint128(-x) : uint128(x);
     }
 
     /// @notice get absolute value of the input, returned as an unsigned number.
     /// @param x: signed number
     /// @return z uint256 absolute value of x
     function abs256(int256 x) internal pure returns (uint256 z) {
+        if (x == type(int256).min) {
+            revert CannotTakeAbsOfMinInt256();
+        }
+
         assembly {
             /// shr(255, x):
             /// shifts the number x to the right by 255 bits:
