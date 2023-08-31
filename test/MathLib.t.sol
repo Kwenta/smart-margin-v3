15c0b6845bd7d8110cf7da9e02d99618b94ac1c0
JaredBorders <jlbxsxs@gmail.com>
2023-08-31 13:24:29 -0400

    ✅ Test abs()

--- test/MathLib.t.sol
+++ test/MathLib.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.18;
 
 import {MathLib} from "src/libraries/MathLib.sol";
 import {Test} from "lib/forge-std/src/Test.sol";
+import {console} from "lib/forge-std/src/console.sol";
 
 contract MathLibTest is Test {
     using MathLib for int128;
@@ -12,39 +13,76 @@ contract MathLibTest is Test {
     function test_abs128() public {
         int128 x = -1 ether;
         uint128 z = x.abs128();
-        assertTrue(z == 1 ether);
+        assertEq(z, 1 ether);
+
+        x = type(int128).min;
+        z = x.abs128();
+        assertEq(z, 2 ** 127);
+
+        x = type(int128).max;
+        z = x.abs128();
+        assertEq(z, (2 ** 127) - 1);
+
+        int128 y = (2 ** 127) - 1;
+        assertEq(x, y);
     }
 
-    function test_abs128_min() public {
-        int128 x = type(int128).min;
+    function test_fuzz_abs128(int128 x) public {
+        uint128 z;
 
-        vm.expectRevert(
-            abi.encodeWithSelector(MathLib.CannotTakeAbsOfMinInt128.selector)
-        );
+        if (x == type(int128).min) {
+            z = x.abs128();
+            assertEq(z, 2 ** 127);
+        } else if (x == type(int128).max) {
+            z = x.abs128();
+            assertEq(z, (2 ** 127) - 1);
+        } else {
+            z = x.abs128();
+            x = x < 0 ? -x : x;
+            uint128 y = uint128(x);
 
-        x.abs128();
+            assertEq(z, y);
+        }
     }
 
     function test_abs256() public {
         int256 x = -1 ether;
         uint256 z = x.abs256();
-        assertTrue(z == 1 ether);
+        assertEq(z, 1 ether);
+
+        x = type(int256).min;
+        z = x.abs256();
+        assertEq(z, 2 ** 255);
+
+        x = type(int256).max;
+        z = x.abs256();
+        assertEq(z, (2 ** 255) - 1);
+
+        int256 y = (2 ** 255) - 1;
+        assertEq(x, y);
     }
 
-    function test_abs256_min() public {
-        int256 x = type(int256).min;
+    function test_fuzz_abs256(int256 x) public {
+        uint256 z;
 
-        vm.expectRevert(
-            abi.encodeWithSelector(MathLib.CannotTakeAbsOfMinInt256.selector)
-        );
-
-        x.abs256();
+        if (x == type(int256).min) {
+            z = x.abs256();
+            assertEq(z, 2 ** 255);
+        } else if (x == type(int256).max) {
+            z = x.abs256();
+            assertEq(z, (2 ** 255) - 1);
+        } else {
+            z = x.abs256();
+            x = x < 0 ? -x : x;
+            uint256 y = uint256(x);
+            assertEq(z, y);
+        }
     }
 
     function test_castU128() public {
         uint256 x = 1 ether;
         uint128 z = x.castU128();
-        assertTrue(z == 1 ether);
+        assertEq(z, 1 ether);
     }
 
     function test_castU128_overflow() public {
