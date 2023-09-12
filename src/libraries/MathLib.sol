bdf2ee11709ac8817710fee0f872ec7f24d81370
JaredBorders <jlbxsxs@gmail.com>
2023-09-12 16:01:35 -0400

    👷🏻‍♂️ MLB-02C: Optimize abs128()

--- src/libraries/MathLib.sol
+++ src/libraries/MathLib.sol
@@ -11,27 +11,16 @@ library MathLib {
     /// @return z uint128 absolute value of x
     function abs128(int128 x) internal pure returns (uint128 z) {
         assembly {
-            /// shl(128, x):
-            /// shifts the number x to the left by 128 bits
-            let y := shl(128, x)
+            /// shr(127, x):
+            /// shifts the number x to the right by 127 bits:
+            /// IF the number is negative, the leftmost bit (bit 127) will be 1
+            /// IF the number is positive,the leftmost bit (bit 127) will be 0
+            let y := shr(127, x)
 
-            /// shr(255, x):
-            /// shifts the number x to the right by 255 bits:
-            /// IF the number is negative, the leftmost bit (bit 255) will be 1
-            /// IF the number is positive,the leftmost bit (bit 255) will be 0
-
-            /// sub(0, shr(255, x)):
-            /// creates a mask of all 1s if x is negative
-            /// creates a mask of all 0s if x is positive
-            let mask := sub(0, shr(255, y))
-
-            /// If x is negative, this effectively negates the number
-            // if x is positive, it leaves the number unchanged, thereby computing the absolute value
-            z := xor(mask, add(mask, y))
-
-            /// shr(128, z):
-            /// shifts the number z to the right by 128 bits after computing the absolute value
-            z := shr(128, z)
+            /// sub(xor(x, y), y):
+            /// IF x is negative, this effectively negates the number
+            /// IF x is positive, it leaves the number unchanged, thereby computing the absolute value
+            z := sub(xor(x, y), y)
         }
     }
 
