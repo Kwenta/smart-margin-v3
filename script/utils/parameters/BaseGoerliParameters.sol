9f1edf605d204a6b6cbb1f57a0c09d31371cc241
JaredBorders <jlbxsxs@gmail.com>
2023-09-19 16:28:17 -0400

    🚀 Update Script for Base Deployment

---
+++ script/utils/parameters/BaseGoerliParameters.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.18;
+
+contract BaseGoerliParameters {
+    address public constant PERPS_MARKET_PROXY =
+        0x9863Dae3f4b5F4Ffe3A841a21565d57F2BA10E87;
+
+    address public constant SPOT_MARKET_PROXY =
+        0x17633A63083dbd4941891F87Bdf31B896e91e2B9;
+
+    address public constant USD_PROXY =
+        0xe487Ad4291019b33e2230F8E2FB1fb6490325260;
+
+    address public constant PYTH = 0x5955C1478F0dAD753C7E2B4dD1b4bC530C64749f;
+}
