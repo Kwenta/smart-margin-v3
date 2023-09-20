02a8359425ed1e9ff1b9eb8d3b8666848c96fa13
JaredBorders <jlbxsxs@gmail.com>
2023-09-20 13:03:59 -0400

    🚀 Deploy Base Goerli

--- script/utils/parameters/BaseGoerliParameters.sol
+++ script/utils/parameters/BaseGoerliParameters.sol
@@ -9,7 +9,7 @@ contract BaseGoerliParameters {
         0x17633A63083dbd4941891F87Bdf31B896e91e2B9;
 
     address public constant USD_PROXY =
-        0xe487Ad4291019b33e2230F8E2FB1fb6490325260;
+        0x579c612E4Bf390f5504DB9f76b6F5759A3172279;
 
     address public constant PYTH = 0x5955C1478F0dAD753C7E2B4dD1b4bC530C64749f;
 }
