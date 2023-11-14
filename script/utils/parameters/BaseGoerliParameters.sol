9c49ce54fb1e8fea9eebd6bb3df949a154c43326
JaredBorders <jaredborders@pm.me>
2023-11-14 11:39:24 -0500

    🚀 Update parameters

--- script/utils/parameters/BaseGoerliParameters.sol
+++ script/utils/parameters/BaseGoerliParameters.sol
@@ -12,4 +12,13 @@ contract BaseGoerliParameters {
         0x579c612E4Bf390f5504DB9f76b6F5759A3172279;
 
     address public constant PYTH = 0x5955C1478F0dAD753C7E2B4dD1b4bC530C64749f;
+
+    address public constant PERPS_MARKET_PROXY_ANDROMEDA =
+        0xEED61f0CB02f3B38923b1b6EAa939D5f04f431b6;
+
+    address public constant SPOT_MARKET_PROXY_ANDROMEDA =
+        0x41A883a85b1AdE59F41d459Fa550b40fa56429DB;
+
+    address public constant USD_PROXY_ANDROMEDA =
+        0xC9ee9628f23b14483EA413C28712690E8D2dC6a3;
 }
