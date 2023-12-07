e811876d9035a0f1ac3cb67e78e2dded90d07dc4
JaredBorders <jaredborders@pm.me>
2023-12-07 18:56:56 -0500

    👷🏻‍♂️ Refactor Trusted Forwarder

--- src/utils/TrustedForwarder.sol
+++ src/utils/TrustedForwarder.sol
@@ -8,22 +8,13 @@ import {ERC2771Context} from
 /// @custom:derived from github.com/Synthetixio/trusted-multicall-forwarder
 /// @author Jared Borders <jaredborders@pm.me>
 contract TrustedForwarder {
-    struct Call3Value {
+    struct Call {
         /// @notice The address called
         address target;
-        /// @notice if true, then an unsuccessful call will revert the whole transaction
-        /// @dev if false, and value is non-zero, then an unsuccessful call may result in trapped ETH
-        /// @custom:recommendation set to true if the call value is non-zero
-        bool requireSuccess;
         /// @notice the allocated value for the call defined by this struct
         uint256 value;
         /// @notice the call data for the call defined by this struct
         bytes callData;
-    }
-
-    struct Result {
-        bool success;
-        bytes returnData;
     }
 
     /// @dev thrown when requestedValue doesn't match with the available msgValue
@@ -34,20 +25,20 @@ contract TrustedForwarder {
 
     /// @notice Aggregate calls with a msg value
     /// @notice Reverts if msg.value is less than the sum of the call values
-    /// @param calls An array of Call3Value structs
-    /// @return returnData An array of Result structs
-    function aggregate3Value(Call3Value[] calldata calls)
+    /// @param calls An array of Call structs
+    /// @return returnData An array of results from the calls
+    function aggregate(Call[] calldata calls)
         public
         payable
-        returns (Result[] memory returnData)
+        returns (bytes[] memory returnData)
     {
         uint256 valAccumulator;
-        Call3Value calldata calli;
+        Call calldata calli;
 
         uint256 length = calls.length;
-        returnData = new Result[](length);
+        returnData = new bytes[](length);
 
-        for (uint256 i = 0; i < length; i++) {
+        for (uint256 i = 0; i < length;) {
             // define the call object
             calli = calls[i];
 
@@ -56,9 +47,6 @@ contract TrustedForwarder {
             if (!_isTrustedByTarget(calli.target)) {
                 revert TargetNotTrusted(calli.target);
             }
-
-            // define default result to return for this call
-            Result memory result = returnData[i];
 
             // define the allocated value for this call
             uint256 val = calli.value;
@@ -69,18 +57,25 @@ contract TrustedForwarder {
             valAccumulator += val;
 
             // make the call and record the result data and whether it was successful
-            (result.success, result.returnData) = calli.target.call{value: val}(
+            (bool success, bytes memory data) = calli.target.call{value: val}(
                 abi.encodePacked(calli.callData, msg.sender)
             );
 
-            // if the specified call requires successful execution,
-            // and the call was not successful, revert
-            if (calli.requireSuccess && !result.success) {
-                bytes memory revertData = result.returnData;
-                uint256 len = revertData.length;
+            // *ALWAYS* revert if the call was not successful
+            /// @dev prevents ETH from being locked in the contract if call
+            /// with non-zero value reverts
+            if (!success) {
+                bytes memory revertData = data;
+                uint256 len = data.length;
                 assembly {
                     revert(add(revertData, 0x20), len)
                 }
+            }
+
+            returnData[i] = data;
+
+            unchecked {
+                ++i;
             }
         }
 
