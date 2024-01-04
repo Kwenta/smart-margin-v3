11fc969670071d985ce8b88fe9dab541c1aae04b
JaredBorders <jaredborders@pm.me>
2024-01-04 16:16:02 -0500

    ✅ Update error

--- test/utils/errors/SynthetixV3Errors.sol
+++ test/utils/errors/SynthetixV3Errors.sol
@@ -18,7 +18,7 @@ contract SynthetixV3Errors {
         bytes extraData
     );
     error InvalidVerificationResponse();
-    error InvalidSettlementStrategy(uint128 settlementStrategyId);
+    error InvalidSettlementStrategy(uint256 settlementStrategyId);
     error MinimumSettlementAmountNotMet(uint256 minimum, uint256 actual);
     error SettlementStrategyNotFound(SettlementStrategyType strategyType);
     error InvalidFeeCollectorInterface(address invalidFeeCollector);
