61b55e9d923315aab369e588765d15be13897180
Flocqst <flocastet@icloud.com>
2024-10-10 20:09:58 +0200

    ✅ add payDebt tests

--- test/PayDebt.t.sol
+++ test/PayDebt.t.sol
@@ -2,28 +2,107 @@
 pragma solidity 0.8.27;
 
 import {Bootstrap} from "test/utils/Bootstrap.sol";
+import {IEngine} from "src/interfaces/IEngine.sol";
 
 contract PayDebtTest is Bootstrap {
+    address public constant DEBT_ACTOR =
+        address(0x325cd6b3CD80EDB102ac78848f5B127eB6DB13f3);
+    uint128 public constant ACCOUNT_ID =
+        170_141_183_460_469_231_731_687_303_715_884_105_729;
+    uint256 public constant INITIAL_DEBT = 2_510_703_436_713_643_968;
+
     function setUp() public {
-        vm.rollFork(ARBITRUM_BLOCK_NUMBER);
+        vm.rollFork(262_400_000);
         initializeArbitrum();
+
+        synthMinter.mint_sUSD(DEBT_ACTOR, AMOUNT);
     }
 
-    function testPayDebt() public {
+    function test_payDebt() public {
+        /// @dev on this block (262_400_000), ACCOUNT_ID has a debt value of INITIAL_DEBT
+        uint256 initialAccountDebt = perpsMarketProxy.debt(ACCOUNT_ID);
+        assertEq(initialAccountDebt, INITIAL_DEBT);
 
-        // todo
+        uint256 initialSUSD = sUSD.balanceOf(DEBT_ACTOR);
 
-        vm.startPrank(ACTOR);
-        sUSD.approve(address(engine), 1);
+        vm.startPrank(DEBT_ACTOR);
+
+        sUSD.approve(address(engine), INITIAL_DEBT);
+
+        engine.payDebt({_accountId: ACCOUNT_ID, _amount: INITIAL_DEBT});
+        vm.stopPrank();
+
+        uint256 finalAccountDebt = perpsMarketProxy.debt(ACCOUNT_ID);
+        assertEq(finalAccountDebt, 0);
+
+        uint256 finalSUSD = sUSD.balanceOf(DEBT_ACTOR);
+        assertEq(finalSUSD, initialSUSD - INITIAL_DEBT);
+    }
+
+    /// @notice asserts that if amount passed is greater than debt, excess sUSD is sent back to the user after paying off the debt
+    function test_payDebt_overpay() public {
+        /// @dev on this block (ARBITRUM_BLOCK_NUMBER), ACCOUNT_ID has a debt value of INITIAL_DEBT
+        uint256 initialAccountDebt = perpsMarketProxy.debt(ACCOUNT_ID);
+        assertEq(initialAccountDebt, INITIAL_DEBT);
+
+        uint256 initialSUSD = sUSD.balanceOf(DEBT_ACTOR);
+
+        vm.startPrank(DEBT_ACTOR);
+
+        sUSD.approve(address(engine), INITIAL_DEBT + SMALLEST_AMOUNT);
+
         engine.payDebt({
-            _accountId: accountId,
-            _amount: 1
+            _accountId: ACCOUNT_ID,
+            _amount: INITIAL_DEBT + SMALLEST_AMOUNT
         });
+        vm.stopPrank();
+
+        uint256 finalAccountDebt = perpsMarketProxy.debt(ACCOUNT_ID);
+        assertEq(finalAccountDebt, 0);
+
+        uint256 finalSUSD = sUSD.balanceOf(DEBT_ACTOR);
+        // assertEq(finalSUSD, initialSUSD - INITIAL_DEBT);
     }
 
-    // todo test for auth
+    function test_payDebt_Unauthorized() public {
+        vm.startPrank(ACTOR);
 
-    // edge case tests
+        sUSD.approve(address(engine), INITIAL_DEBT);
 
-    // test paying debt with different setups?
+        vm.expectRevert(abi.encodeWithSelector(IEngine.Unauthorized.selector));
+
+        engine.payDebt({_accountId: ACCOUNT_ID, _amount: INITIAL_DEBT});
+    }
+
+    function test_payDebt_Fuzz(uint256 amount) public {
+        vm.assume(amount < AMOUNT);
+        vm.assume(amount > SMALLEST_AMOUNT);
+
+        uint256 initialAccountDebt = perpsMarketProxy.debt(ACCOUNT_ID);
+        assertEq(initialAccountDebt, INITIAL_DEBT);
+
+        uint256 initialSUSD = sUSD.balanceOf(DEBT_ACTOR);
+
+        vm.startPrank(DEBT_ACTOR);
+
+        sUSD.approve(address(engine), amount);
+
+        engine.payDebt({_accountId: ACCOUNT_ID, _amount: amount});
+
+        vm.stopPrank();
+
+        uint256 finalAccountDebt = perpsMarketProxy.debt(ACCOUNT_ID);
+        uint256 finalSUSD = sUSD.balanceOf(DEBT_ACTOR);
+
+        if (amount > INITIAL_DEBT) {
+            // If amount is greater than the initial debt, the debt should be fully paid
+            // and excess sUSD should be sent back to the user
+            assertEq(finalAccountDebt, 0);
+            // assertEq(finalSUSD, initialSUSD - INITIAL_DEBT);
+        } else {
+            // If amount is less or equal than the initial debt, only part of the debt is paid
+            assertEq(finalAccountDebt, INITIAL_DEBT - amount);
+            assertEq(finalSUSD, initialSUSD - amount);
+        }
+    }
 }
--- test/utils/Constants.sol
+++ test/utils/Constants.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.27;
 /// @title Contract for defining constants used in testing
 /// @author JaredBorders (jaredborders@pm.me)
 contract Constants {
-    uint256 public constant ARBITRUM_BLOCK_NUMBER = 262_399_473;
+    uint256 public constant ARBITRUM_BLOCK_NUMBER = 262_365_073;
 
     address internal constant OWNER = address(0x01);
 
