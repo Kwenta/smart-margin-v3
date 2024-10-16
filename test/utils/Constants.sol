29000fafb9768fcfd8cf57c5d8a8e2c80d624df3
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-10-16 11:34:45 -0400

    ✅ new block number

--- test/utils/Constants.sol
+++ test/utils/Constants.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.27;
 /// @title Contract for defining constants used in testing
 /// @author JaredBorders (jaredborders@pm.me)
 contract Constants {
-    uint256 public constant ARBITRUM_BLOCK_NUMBER = 262_365_073;
+    uint256 public constant ARBITRUM_BLOCK_NUMBER = 264_441_861;
 
     address internal constant OWNER = address(0x01);
 
