569de0141251ab9788cc43aaed8474683afc07ff
Flocqst <flocastet@icloud.com>
2024-11-20 17:03:22 +0100

    🚀 deploy zap + engine with usdc unwind flow fix

--- script/utils/parameters/BaseParameters.sol
+++ script/utils/parameters/BaseParameters.sol
@@ -31,5 +31,5 @@ contract BaseParameters {
     // https://usecannon.com/packages/synthetix-spot-market/3.3.5/84531-andromeda
     uint128 public constant SUSDC_SPOT_MARKET_ID = 1;
 
-    address public constant ZAP = 0xab62e32C01d2b38152FC23a912218aad6997a71A;
+    address public constant ZAP = 0xDE5858409c1776e03291242f2430413839Aaa35C;
 }
