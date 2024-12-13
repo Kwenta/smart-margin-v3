781e34c7cefbfd78b240ed8a52a0172e768c6cdf
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-12-12 23:27:15 -0500

    🚀 add pay to deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -8,6 +8,7 @@ import {MulticallerWithSender} from "src/utils/MulticallerWithSender.sol";
 import {BaseParameters} from "script/utils/parameters/BaseParameters.sol";
 import {BaseSepoliaParameters} from
     "script/utils/parameters/BaseSepoliaParameters.sol";
+import {Pay} from "src/utils/Pay.sol";
 
 // forge utils
 import {Script} from "lib/forge-std/src/Script.sol";
@@ -21,6 +22,7 @@ contract Setup is Script {
         address sUSDProxy,
         address pDAO,
         address zap,
+        address payable pay,
         address usdc,
         address weth
     ) public returns (Engine engine) {
@@ -30,6 +32,7 @@ contract Setup is Script {
             _sUSDProxy: sUSDProxy,
             _pDAO: pDAO,
             _zap: zap,
+            _pay: pay,
             _usdc: usdc,
             _weth: weth
         });
@@ -56,6 +59,7 @@ contract DeployBase is Setup, BaseParameters {
             sUSDProxy: USD_PROXY_ANDROMEDA,
             pDAO: PDAO,
             zap: ZAP,
+            pay: PAY,
             usdc: USDC,
             weth: WETH
         });
@@ -78,6 +82,7 @@ contract DeployBaseSepolia is Setup, BaseSepoliaParameters {
             sUSDProxy: USD_PROXY_ANDROMEDA,
             pDAO: PDAO,
             zap: ZAP,
+            pay: PAY,
             usdc: USDC,
             weth: WETH
         });
@@ -95,6 +100,20 @@ contract DeployMulticallBase is Setup, BaseParameters {
         vm.startBroadcast(privateKey);
 
         new MulticallerWithSender();
+
+        vm.stopBroadcast();
+    }
+}
+
+/// @dev steps to deploy and verify on Base:
+/// (1) load the variables in the .env file via `source .env`
+/// (2) run `forge script script/Deploy.s.sol:DeployPay --rpc-url $BASE_RPC_URL --etherscan-api-key $BASESCAN_API_KEY --broadcast --verify -vvvv`
+contract DeployPay is Setup, BaseParameters {
+    function run() public {
+        uint256 privateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(privateKey);
+
+        new Pay(WETH);
 
         vm.stopBroadcast();
     }
