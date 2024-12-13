19cef112cdabea8f95781c5f739215f42082284d
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-12-12 23:23:50 -0500

    👷 pay contract

---
+++ src/utils/Pay.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.27;
+
+import {IWETH} from "src/interfaces/tokens/IWETH.sol";
+
+/// @notice Pay contract for unwrapping WETH and sending it to a recipient
+/// @author cmontecoding
+contract Pay {
+    
+    /// @notice WETH contract
+    IWETH public WETH;
+
+    /// @notice thrown when a call to transfer eth fails
+    error ETHTransferFailed();
+
+    constructor(address _weth) {
+        WETH = IWETH(_weth);
+    }
+
+    /// @notice unwrap WETH and send it to a recipient
+    /// @param amount amount of WETH to unwrap
+    /// @param to recipient address
+    function unwrapAndPay(uint amount, address to) public {
+        WETH.transferFrom(msg.sender, address(this), amount);
+        WETH.withdraw(amount);
+        (bool success, ) = to.call{value: amount}("");
+        if (success != true) {
+            revert ETHTransferFailed();
+        }
+    }
+
+    receive() external payable {}
+
+}
